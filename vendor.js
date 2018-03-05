aa9a3da9263ac069e3d4c760e911ddb5cd9be6d4
xiaolin <462441355@qq.com>
2018-03-05 21:32:02 +0800

    * 增加配置

--- vendor.js
+++ vendor.js
@@ -1,19 +1,25 @@
+// 支持async await
 import regeneratorRuntime from 'regenerator-runtime'
-
 global.regeneratorRuntime = regeneratorRuntime
+
+import _ from 'lodash'
+global._ = _
+
+import R from 'ramda'
+global.R = R
 
 import uiti from './utils/util'
 
 global.uiti = uiti
-
-import R from 'ramda'
-global.R = R
 
 const asyncWrap = fn => (option = {}) => new Promise((resolve, reject) => {
     let conf = {
         success: res => {
             console.log(res)
             resolve(res)
+        },
+        fail: err => {
+            reject(err)
         }
     }
     wx[fn](R.merge(conf, options))
@@ -22,4 +28,31 @@ const asyncWrap = fn => (option = {}) => new Promise((resolve, reject) => {
 wx.getSystemInfoAsync = asyncWrap('getSystemInfo')
 wx.loginAsync = asyncWrap('login')
 wx.getUserInfoAsync = asyncWrap('getUserInfo')
-wx.reqAsync = asyncWrap('request')
+wx.requestAsync = asyncWrap('request')
+
+// 动画
+global.requestAnimationFrame = callback => {
+    const currentTime = new Date().getTime()
+    const timeToCall = Math.max(0, 16 - (currentTime - lastTime))
+    const timer = global.setTimeout(function () {
+        callback(currentTime + timeToCall)
+    }, timeToCall)
+
+    lastTime = currentTime + timeToCall
+
+    return timer
+}
+
+global.cancelAnimationFrame = timer => {
+    clearTimeout(timer)
+}
+
+import TWEEN from 'tween.js'
+
+
+TWEEN.now = function () {
+    return new Date().getTime()
+}
+
+
+global.TWEEN = TWEEN
